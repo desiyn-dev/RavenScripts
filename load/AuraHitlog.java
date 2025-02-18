5585d677b7bf8d1f101832e8c751c340a21c654f
desiyn-dev <m@afterlike.org>
2025-02-18 12:50:34 -0500

    deprecate aura hitlog script

--- load/AuraHitlog.java
+++ load/AuraHitlog.java
@@ -4,5 +4,5 @@ void onLoad() {
 
 void onEnable() {
     client.print("&7[&dR&7] &fAuraHitlog has been replaced with the new &lUtilities&f script.");
-    client.print("&7[&dR&7] &ahttps://raw.githubusercontent.com/desiyn-dev/RavenScripts/main/download/Utilities.java");
+    client.print("https://raw.githubusercontent.com/desiyn-dev/RavenScripts/main/download/Utilities.java");
 }
