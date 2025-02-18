3f31cbdc2e183ae7939e1a8866a4caaa3e86426b
desiyn-dev <m@afterlike.org>
2025-02-18 16:22:56 -0500

    update utilities download

--- download/Utilities.java
+++ download/Utilities.java
@@ -1,2 +1,10 @@
+static final String[] TRACKED_MODULES = {
+    "KillAura", "Bhop",
+    "KeepSprint", "Scaffold",
+    "Long Jump", "Safewalk",
+    "NoFall", "NoSlow",
+    "BlinkNoFall", "AntiKnockback"
+};
+
 load - "https://raw.githubusercontent.com/desiyn-dev/RavenScripts/refs/heads/main/load/ColorHandler.java"
 load - "https://raw.githubusercontent.com/desiyn-dev/RavenScripts/refs/heads/main/load/Utilities.java"
